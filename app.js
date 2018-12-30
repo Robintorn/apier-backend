42e398d155571e8593b2f05674f4e5733f77e70b
robin torn <robin.torn.98@gmail.com>
2018-12-30 18:51:55 +0100

    tjena

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 mongodb.connect('mongodb://robin:7w83LnnuP6XedADg@cluster0-shard-00-00-opstr.mongodb.net:27017,cluster0-shard-00-01-opstr.mongodb.net:27017,cluster0-shard-00-02-opstr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
